f44bbfd5bea92eba4f19513ab4e3e540f97566f0
Prabhjot kaur <Prabhjot.Kaur2@metropolia.fi>
2024-10-05 18:59:39 +0300

    Added third card to App.jsx

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import Button from './components/Button';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="content">
+        <Card title="Card Title 1" content="This is the content of the first card." />
+        <Card title="Card Title 2" content="This is the content of the second card." />
+        <Card title="Card Title 3" content="This is the content of the third card." />  {/* New card */}
+        <Button label="Click Me" onClick={handleClick} />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+
+export default App;
